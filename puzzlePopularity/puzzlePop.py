686f94663451672738c069adbfa30a4d7ec216e0
Julian Kern <juliankern323@gmail.com>
2023-12-08 17:02:13 +0100

    some minor test with pandas

--- puzzlePopularity/puzzlePop.py
+++ puzzlePopularity/puzzlePop.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 
 if __name__=='__main__':
-    puzzleDB = '~/chess/resources/lichess_db_puzzle.csv'
+    puzzleDB = '~/chess/resources/lichess_db_puzzle_100k.csv'
     df = pd.read_csv(puzzleDB)
+    df = df.sort_values('Popularity', ascending=False)
+    pd.set_option('display.max_columns', None)
     print(df.info())
+    print(df)
