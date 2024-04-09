6018366963881d14b781b5bb8f7f05e4340667a3
Julian <juliankern313@gmail.com>
2024-04-09 21:10:22 +0200

    added method to compare policy of different networks

--- gameAnalysis/brilliancy.py
+++ gameAnalysis/brilliancy.py
@@ -20,6 +20,7 @@ def getPolicyHead(leela: engine, positions: list) -> list:
         board = chess.Board(pos)
         info = leela.analysis(board, chess.engine.Limit(nodes=1))
         for i in info:
+            # print(i)
             for j in i.values():
                 p = re.findall('P: *\d*\.\d\d%', str(j))
                 if (move := str(j).split()[0]) != 'node' and p:
@@ -29,9 +30,28 @@ def getPolicyHead(leela: engine, positions: list) -> list:
     return policies
 
 
+def testNets(nets: list, positions: list) -> dict:
+    """
+    This function tests the policy of different nets for different positions
+    nets: list
+        The list of different nets
+    positions: list
+        A list of FEN strings containing the positions
+    return -> dict
+        A dictionary with the nets as keys and the list of policies as values
+    """
+    pol = dict()
+    for net in nets:
+        leela = configureEngine('lc0', {'WeightsFile': net, 'UCI_ShowWDL': 'true', 'VerboseMoveStats': 'ture'})
+        pol[net] = getPolicyHead(leela, positions)
+        leela.quit()
+    return pol
+
+
 if __name__ == '__main__':
     weights = '/home/julian/Desktop/largeNet'
-    leela = configureEngine('lc0', {'WeightsFile': weights, 'UCI_ShowWDL': 'true', 'VerboseMoveStats': 'true'})
-    positions = ['r2q1rk1/1pp1bppp/p1n5/3np3/PPB1N1b1/2PP1N2/2Q2PPP/R1B2RK1 w - - 5 14']
-    print(getPolicyHead(leela, positions))
-    leela.quit()
+    # leela = configureEngine('lc0', {'WeightsFile': weights, 'UCI_ShowWDL': 'true', 'VerboseMoveStats': 'true'})
+    positions = ['rn3rk1/pp3ppp/2pbB3/q7/3P4/2P2N2/P4PPP/R2Q1RK1 w - - 2 16', '4r3/5pk1/ppn1bQ1p/3pPp1P/1q1P1N2/4P3/4N1PK/8 b - - 1 2']
+    networks = ['/home/julian/Desktop/smallNet.gz', '/home/julian/Desktop/mediumNet.gz', '/home/julian/Desktop/largeNet', '/home/julian/chess/maiaNets/maia-1900.pb']
+    print(testNets(networks, positions))
+    # leela.quit()
