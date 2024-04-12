c1d85d70a14f9cee4d3d697f2fe46cf1606095a2
Julian Kern <juliankern323@gmail.com>
2024-04-12 06:58:38 +0200

    some changes to puzzles

--- puzzlePopularity/puzzlePop.py
+++ puzzlePopularity/puzzlePop.py
@@ -7,10 +7,12 @@ if __name__=='__main__':
     # df = df.sort_values('Popularity', ascending=False)
     pd.set_option('display.max_columns', None)
 
-    plays = 500
+    plays = 1000
     ndf = df[df['NbPlays'] >= plays]
     ndf = ndf.reset_index()
     ndf['Solution Length'] = [len(sol.split(' ')) for sol in ndf['Moves']]
+    for i in ndf.index:
+        print(ndf['FEN'][i], ndf['Moves'][i])
     print(ndf.sort_values('Solution Length'))
     print(ndf['Popularity'].corr(ndf['Rating']))
     print(ndf['NbPlays'].corr(ndf['Rating']))
