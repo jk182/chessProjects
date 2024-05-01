81c578c9fba2866bdd592a71a9ecfd724cfb54a5
Julian <juliankern313@gmail.com>
2024-05-01 16:07:10 +0200

    added plotting for puzzle performance

--- maiaExperiments/puzzlePerf.py
+++ maiaExperiments/puzzlePerf.py
@@ -5,6 +5,7 @@ import functions
 
 import chess
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def reduceDataset(puzzles: str, minPlays: int):
@@ -65,12 +66,12 @@ def solvePuzzle(engine: chess.engine, FEN: str, solution: str, nodes: int = 1, s
         board.push_uci(moves[0])
         moves = moves[1:]
 
-    white = board.turn()
+    white = board.turn
     for move in moves:
         info = engine.analyse(board, chess.engine.Limit(nodes=nodes))
         engMove = str(info['pv'][0])
         # Check if we are actually trying to find the best move
-        if white == board.turn():
+        if white == board.turn:
             if engMove == move:
                 ret = 1
             else:
@@ -107,10 +108,58 @@ def calcPerformanceRating(engine: chess.engine, df) -> tuple:
     return (sum(partialSol)/len(partialSol), sum(fullSol)/len(fullSol))
 
 
+def calcPuzzleScore(engine: chess.engine, df) -> tuple:
+    """
+    This function calculates the score (0-1) for an engine on a given set of puzzles
+    engine: chess.engine
+        A configured chess engine
+    df
+        Dataframe containing the puzzles
+    return -> tuple
+        A tuple containing the score (from 0 to 1) for partial and full solutions
+    """
+    partialSol = 0
+    fullSol = 0
+    for i in df.index:
+        score = solvePuzzle(engine, df['FEN'][i], df['Moves'][i])
+        if score == 1:
+            partialSol += 1
+        if score == 2:
+            fullSol += 1
+    return ((partialSol+fullSol)/len(df.index), fullSol/len(df.index))
+
+
+def plotPuzzlePerformance(puzzlePerf: dict, engineNames: list):
+    """
+    This function plots the performance of different engines on various datasets
+    puzzlePerf: dict
+        This dictionary has the name of the puzzle dataset as key (e.g. 1500-1800 puzzles) and a list of the 
+        scores from the engines as values.
+    engineNames: list
+        The names of the engines, sorted the same way as in the values of the puzzlePerf dictionary
+    """
+    fig, ax = plt.subplots()
+    ax.set_facecolor('#e6f7f2')
+
+    plt.xticks(ticks=range(1, len(engineNames)+1), labels=engineNames)
+    colors = [('#6096B4', '#93BFCF'), ('#FF87CA', '#FFC4E1'), ('#7ED3B2', '#B9E6D3')]
+
+    width = len(engineNames) / (4 * (len(puzzlePerf.keys())+1))
+    offset = -width*(len(puzzlePerf.keys())-0.5)
+
+    for puz, perf in puzzlePerf.items():
+        colorIndex = list(puzzlePerf.keys()).index(puz) % len(colors)
+        ax.bar([ i+1+offset for i in range(len(perf)) ], [ p[0] for p in perf ], color=colors[colorIndex][0], edgecolor='black', linewidth=0.5, width=width, label=f'{puz} (partial solution)')
+        ax.bar([ i+1+offset+width for i in range(len(perf)) ], [ p[1] for p in perf ], color=colors[colorIndex][1], edgecolor='black', linewidth=0.5, width=width, label=f'{puz} (full solution)')
+        offset += 2*width
+    ax.legend()
+
+    plt.show()
+
 
 if __name__=='__main__':
     puzzleDB = '~/chess/resources/lichess_db_puzzle.csv'
-    plays = 20000
+    plays = 30000
     # reduceDataset(puzzleDB, plays)
     newPDB = f'{puzzleDB[:-4]}-{plays}.csv'
     df = pd.read_csv(newPDB)
@@ -118,13 +167,26 @@ if __name__=='__main__':
     # pd.set_option('display.max_columns', None)
     print(df)
 
-    ratings = [0, 1000, 1400, 1800, 2200, 2600, 4000]
-    for i in range(len(ratings)-1):
-        print(ratingBand(df, ratings[i], ratings[i+1]))
 
     maia1100 = functions.configureEngine('lc0', {'WeightsFile': '/home/julian/chess/maiaNets/maia-1100.pb', 'UCI_ShowWDL': 'true'})
     maia1900 = functions.configureEngine('lc0', {'WeightsFile': '/home/julian/chess/maiaNets/maia-1900.pb', 'UCI_ShowWDL': 'true'})
-    # print(calcPerformanceRating(maia1100, df))
-    # print(calcPerformanceRating(maia1900, df))
+
+    engineNames = ['Maia 1100', 'Maia 1900']
+    engines = [maia1100, maia1900]
+
+    puzzlePerf = dict()
+
+    ratings = [0, 1200, 1500, 1800, 2100, 2400, 4000]
+    for i in range(len(ratings)-1):
+        lower = ratings[i]
+        upper = ratings[i+1]
+        bandDF = ratingBand(df, lower, upper)
+        
+        puzzlePerf[f'{lower}-{upper}'] = list()
+        for e in engines:
+            puzzlePerf[f'{lower}-{upper}'].append(calcPuzzleScore(e, bandDF))
+
+    plotPuzzlePerformance(puzzlePerf, engineNames)
+
     maia1100.quit()
     maia1900.quit()
