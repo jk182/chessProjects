578bb0b05c0f19f70a8c9ed6bfceabec8af9c2aa
Julian Kern <juliankern323@gmail.com>
2024-07-12 12:45:26 +0200

    added plotting of the evaluation graphs

--- dynamics/dynamics.py
+++ dynamics/dynamics.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt
 import chess
 import os, sys
 import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import functions
+
 
 
 def depthChange(fen: str, engine: chess.engine, minDepth: int = 1, maxDepth: int = 18) -> list:
@@ -91,6 +93,36 @@ def testLichessPuzzles(dbPath: str, limit: int = None) -> list:
     return failed
 
 
+def plotEvalChange(fens: list, engine: chess.engine, minDepth: int = 1, maxDepth: int = 20, filename: str = None):
+    """
+    This function plots the change in evaluation for various depths in the given FENs
+    """
+    mateScore = 10000
+    colors = ['#689bf2', '#f8a978', '#ff87ca', '#beadfa']
+    labels = ['Position 1', 'Position 2', 'Starting Position']
+    fig, ax = plt.subplots(figsize=(10,6))
+    ax.set_facecolor('#e6f7f2')
+
+    for i, fen in enumerate(fens):
+        evals = depthChange(fen, engine, minDepth, maxDepth)
+        ax.plot(range(minDepth, maxDepth+1), [s.score(mate_score=mateScore) for s in evals], color=colors[i], label=labels[i])
+
+    ax.set_xlim(minDepth, maxDepth)
+    ax.set_ylim(0)
+    ax.set_xticks(range(minDepth, maxDepth+1))
+    ax.set_xlabel('Depth')
+    ax.set_ylabel('Evaluation')
+    plt.subplots_adjust(bottom=0.1, top=0.95, left=0.1, right=0.95)
+    plt.title('Evaluation at different Depths')
+    ax.legend()
+
+    if filename:
+        plt.savefig(filename, dpi=500)
+    else:
+        plt.show()
+
+
+
 if __name__ == '__main__':
     sf = functions.configureEngine('stockfish', {'Threads': 10})
     fens = ['r1bq1rk1/2ppbppp/p1n2n2/1p2p3/4P3/1B3N2/PPPP1PPP/RNBQR1K1 w - - 2 8',
@@ -99,6 +131,8 @@ if __name__ == '__main__':
             'r2q1r2/p3bkp1/b4n1p/2pn4/3p4/1P3NPB/P1QBPP1P/R3R1K1 w - - 0 20']
     puzzleDB = '~/chess/resources/lichess_db_puzzle-10000.csv'
     # print(findPuzzles('/Users/julian/Desktop/testPGN.pgn', sf))
-    print(testLichessPuzzles(puzzleDB, 2000))
+    # print(testLichessPuzzles(puzzleDB, 2000))
     # print(findPuzzles('../resources/jk_200.pgn', sf))
+    # plotEvalChange(['4r2k/pp4pp/7q/3Npr2/2Pn1p2/PP3P2/6PP/1Q1RR2K w - - 6 29', '2rqk2r/1p2ppb1/p2p1Bp1/3b3p/4P3/1B3P2/PPPQ2PP/2KR3R b k - 0 14', 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'], sf, filename='../out/evalDepth.png')
+    plotEvalChange(['8/7r/8/5kP1/8/8/5PK1/1R6 w - - 0 57'], sf, maxDepth=30, filename='../out/evalDepth2.png')
     sf.quit()
