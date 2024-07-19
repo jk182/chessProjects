762e6e09a9eb970a4e30759acfac8262942472e7
Julian <juliankern313@gmail.com>
2024-07-19 18:29:48 +0200

    changes for publication

--- WDLprediction/WDLprediction.py
+++ WDLprediction/WDLprediction.py
@@ -1,10 +1,13 @@
 import json
+import statistics
+import pickle
 import requests
 import pandas as pd
 import chess
 from chess import engine
 import matplotlib.pyplot as plt
 import os, sys
+import time
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import functions
@@ -17,6 +20,10 @@ def getExplorerInfo(FEN: str, speeds: list = ['blitz', 'rapid', 'classical'], ra
     speedsStr = str(speeds)[1:-1].replace(' ', '').replace("'", "")
     ratingsStr = str(ratings)[1:-1].replace(' ', '')
     rq = requests.get(f'https://explorer.lichess.ovh/lichess?variant=standard&speeds={speedsStr}&ratings={ratingsStr}&fen={FEN}')
+    while rq.status_code == 429:
+        time.sleep(10)
+        print('Too many requests!')
+        rq = requests.get(f'https://explorer.lichess.ovh/lichess?variant=standard&speeds={speedsStr}&ratings={ratingsStr}&fen={FEN}')
     res = json.loads(rq.text)
     return res
 
@@ -65,15 +72,16 @@ def getEngineWDL(engine: chess.engine, FEN: str, nodes: int = 10000) -> list:
     return wdl
 
 
-def compareEngines(engines: list, fens: list) -> list:
+def compareEngines(engines: list, fens: list, ratings: list) -> list:
     """
     This function calculates the score of various engines for various poisitions
     """
-    minGames = 300
+    minGames = 500
     scores = [ list() for i in range(len(engines)+1) ]
     for fen in fens:
-        expInfo = getExplorerInfo(fen)
+        expInfo = getExplorerInfo(fen, ratings=ratings)
         if expInfo['white']+expInfo['black']+expInfo['draws'] < minGames:
+            time.sleep(1)
             continue
         scores[-1].append(calcScoreWDL(getExplorerWDL(expInfo)))
         for i, engine in enumerate(engines):
@@ -86,18 +94,19 @@ def genPositions(pgnPath: str) -> list:
     This function generates a list of FENs in order to compare the different engines on these positions
     """
     fens = list()
-    with open(pgnPath, 'r') as pgn:
+    with open(pgnPath, 'r', encoding="windows-1252") as pgn:
         while game := chess.pgn.read_game(pgn):
             board = game.board()
             for i, move in enumerate(game.mainline_moves()):
                 if i == 18:
-                    fens.append(board.fen())
-                    break
+                    if (fen := board.fen()) not in fens:
+                        fens.append(board.fen())
+                        break
                 board.push(move)
     return fens
 
 
-def plotScoreDifferences(scores: list, filename: str = None):
+def plotScoreDifferences(scores: list, labels: list, filename: str = None):
     """
     This function plotes the differences in scores between engines and the Lichess database
     It is assumed that the last entry in scores is from the Lichess database
@@ -110,10 +119,13 @@ def plotScoreDifferences(scores: list, filename: str = None):
 
     for j, s in enumerate(scores[:-1]):
         y = [ abs(s[i]-db[i]) for i in range(len(s)) if db[i] >= 0 ]
-        ax.plot(range(len(y)), list(reversed(sorted(y))), color=colors[j])
+        ax.plot(range(len(y)), y, color=colors[j], label=labels[j])
 
     plt.subplots_adjust(bottom=0.1, top=0.95, left=0.1, right=0.95)
-    plt.title('Accuracy Distribution')
+    plt.title('Difference between expected and actual scores')
+    ax.set_xlim(0, len(y)-1)
+    ax.set_ylim(0)
+    ax.set_ylabel('Difference to actual score')
     ax.legend()
 
     if filename:
@@ -122,16 +134,49 @@ def plotScoreDifferences(scores: list, filename: str = None):
         plt.show()
 
 
+def analyseScores(filenames: list):
+    """
+    Function to analyse the difference in scores
+    """
+    for filename in filenames:
+        print(filename)
+        with open(filename, 'rb') as f:
+            scores = pickle.load(f)
+        for i in range(len(scores)-1):
+            x = [ abs(scores[i][j]-scores[-1][j]) for j in range(len(scores[i])) ]
+            print(statistics.median(x))
+            print(sum(x)/len(x))
+
+
 if __name__ == '__main__':
-    d = getExplorerInfo('rnbqk2r/ppp1bppp/4pn2/3p4/2PP4/6P1/PPQBPPBP/RN2K1NR b KQkq - 5 6')
-    printWDL(d)
-    leela = functions.configureEngine('lc0', {'WeightsFile': '/home/julian/Desktop/largeNet', 'UCI_ShowWDL': 'true', 'WDLCalibrationElo': '2000', 'Contempt': '0', 'ContemptMode': 'white_side_analysis', 'WDLEvalObjectivity': '1.0', 'ScoreType': 'WDL_mu'})
-    maia = functions.configureEngine('lc0', {'WeightsFile': '/home/julian/chess/maiaNets/maia-1900.pb', 'UCI_ShowWDL': 'true'})
-    positions = ['r1bq1rk1/2ppbppp/p1n2n2/1p2p3/3PP3/1B3N2/PPP2PPP/RNBQR1K1 b - - 0 8',
-            'rnbqkbnr/pppp1ppp/8/4p3/4PP2/8/PPPP2PP/RNBQKBNR b KQkq - 0 2',
-            'rnb1kb1r/1p3ppp/p2ppn2/6B1/3NPP2/q1N5/P1PQ2PP/1R2KB1R w Kkq - 2 10']
-    fens = genPositions('../resources/2700games2023.pgn')
-    scores = compareEngines([leela, maia], fens)
-    plotScoreDifferences(scores)
+    # fens = genPositions('../resources/2650games2023.pgn')
+    # print(fens[:30])
+    analyseScores(['../out/WDL1500', '../out/WDL2000', '../out/WDL2500'])
+    with open('../out/WDL2500', 'rb') as f:
+        scores2500 = pickle.load(f)
+    plotScoreDifferences(scores2500, ['LC0 WDL\nContempt 2500'], '../out/expectedScore2500.png')
+    
+    """
+    leela = functions.configureEngine('lc0', {'Threads': '6', 'WeightsFile': '/home/julian/Desktop/largeNet', 'UCI_ShowWDL': 'true', 'WDLCalibrationElo': '1500', 'Contempt': '0', 'ContemptMode': 'white_side_analysis', 'WDLEvalObjectivity': '1.0', 'ScoreType': 'WDL_mu'})
+    maia = functions.configureEngine('lc0', {'Threads': '6', 'WeightsFile': '/home/julian/chess/maiaNets/maia-1500.pb', 'UCI_ShowWDL': 'true'})
+    scores = compareEngines([leela, maia], fens, [1400])
+    with open('../out/WDL1500', 'wb+') as f:
+        pickle.dump(scores, f)
     leela.quit()
     maia.quit()
+    leela = functions.configureEngine('lc0', {'WeightsFile': '/home/julian/Desktop/largeNet', 'UCI_ShowWDL': 'true', 'WDLCalibrationElo': '2000', 'Contempt': '0', 'ContemptMode': 'white_side_analysis', 'WDLEvalObjectivity': '1.0', 'ScoreType': 'WDL_mu'})
+    maia = functions.configureEngine('lc0', {'WeightsFile': '/home/julian/chess/maiaNets/maia-1900.pb', 'UCI_ShowWDL': 'true'})
+    scores = compareEngines([leela, maia], fens, [1800, 2000])
+    with open('../out/WDL2000', 'wb+') as f:
+        pickle.dump(scores, f)
+    leela.quit()
+    maia.quit()
+    # with open('../out/WDL2000', 'rb') as f:
+    #     scores = pickle.load(f)
+    leela = functions.configureEngine('lc0', {'WeightsFile': '/home/julian/Desktop/largeNet', 'UCI_ShowWDL': 'true', 'WDLCalibrationElo': '2500', 'Contempt': '0', 'ContemptMode': 'white_side_analysis', 'WDLEvalObjectivity': '1.0', 'ScoreType': 'WDL_mu'})
+    scores = compareEngines([leela], fens, [2500])
+    with open('../out/WDL2500', 'wb+') as f:
+        pickle.dump(scores, f)
+    plotScoreDifferences(scores, ['LC0 WDL\nContempt 2000', 'Maia 1900'])
+    leela.quit()
+    """
