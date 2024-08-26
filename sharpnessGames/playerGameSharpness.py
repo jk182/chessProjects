d47554c5039e4e150b41e3adbbd3b5758e7fa521
Julian Kern <juliankern323@gmail.com>
2024-08-26 18:03:09 +0200

    game sharpness publishing

--- sharpnessGames/playerGameSharpness.py
+++ sharpnessGames/playerGameSharpness.py
@@ -36,7 +36,7 @@ def sharpChangeByColor(pgnPaths: list(), onlyArm: bool = True) -> dict:
                     if c := functions.readComment(node, True, True):
                         csharp = functions.sharpnessLC0(c[0]) * ((1000-max(c[0]))/1000)
                     
-                    diff = float(csharp-lastSharp)
+                    diff = max(-3, min(3, float(csharp-lastSharp)))
                     lastSharp = csharp
 
                     if not node.turn():
@@ -81,6 +81,8 @@ def sharpChangeForPlayer(pgnPaths: list(), player: str) -> list:
                     node = node.variations[0]
                     if c := functions.readComment(node, True, True):
                         csharp = functions.sharpnessLC0(c[0]) * ((1000-max(c[0]))/1000)
+                        if max(c[0]) >= 800:
+                            csharp = 0
                         ceval = int(c[1])
                     else:
                         continue
@@ -113,7 +115,7 @@ def getAvgSharpChange(sharpChange: dict, maxMove: int = None) -> tuple:
     return (sum(w)/len(w), sum(b)/len(b))
 
 
-def plotGameSharpness(pgnPath: str):
+def plotGameSharpness(pgnPath: str, filename: str = None):
     """
     This function plots the sharpness change of a single game, move by move
     pgnPath: str
@@ -125,21 +127,25 @@ def plotGameSharpness(pgnPath: str):
 
     fig, ax = plt.subplots(figsize=(10,6))
 
-    ax.plot(range(len(white)), [min(w[0], 5) for w in white], color='#f8a978', label='White sharpness change')
-    ax.plot(range(len(black)), [min(b[0], 5) for b in black], color='#111111', label='Black sharpness change')
+    ax.plot(range(1, len(white)+1), [min(w[0], 5) for w in white], color='#f8a978', label='White sharpness change')
+    ax.plot(range(1, len(black)+1), [min(b[0], 5) for b in black], color='#111111', label='Black sharpness change')
 
+    plt.axhline(0, color='black', linewidth=0.5)
     ax.set_facecolor('#e6f7f2')
     ax.set_xlabel('Move Number')
     ax.set_ylabel('Sharpness Change')
-    ax.set_xlim(0, len(white))
+    ax.set_xlim(1, len(white)+1)
     plt.subplots_adjust(bottom=0.1, top=0.95, left=0.1, right=0.95)
 
     ax.legend()
 
-    plt.show()
+    if filename:
+        plt.savefig(filename, dpi=500)
+    else:
+        plt.show()
 
 
-def plotPlayerSharpness(sharpChange: list):
+def plotPlayerSharpness(sharpChange: list, filename: str = None):
     """
     This function plots the average sharpness change of a player per move for White and Black
     sharpChange: list
@@ -153,30 +159,59 @@ def plotPlayerSharpness(sharpChange: list):
 
     maxBMoves = max([b[1] for b in black])
     bMoveAvg = [sum([b[0] for b in black if b[1] == move])/len([b[0] for b in black if b[1] == move]) for move in range(1, maxBMoves+1)]
-    print([b[0] for b in black if b[1] == 41])
-    print(bMoveAvg[40])
 
     fig, ax = plt.subplots(figsize=(10, 6))
 
-    ax.plot(range(len(wMoveAvg)), wMoveAvg, color='#f8a978')
-    ax.plot(range(len(bMoveAvg)), bMoveAvg, color='#111111')
+    ax.plot(range(1, len(wMoveAvg)+1), wMoveAvg, color='#f8a978')
+    ax.plot(range(1, len(bMoveAvg)+1), bMoveAvg, color='#111111')
     plt.axhline(0, color='black', linewidth=0.5)
     
     ax.set_facecolor('#e6f7f2')
-    ax.set_xlim(0, 40)
+    ax.set_xlim(1, 40)
     plt.subplots_adjust(bottom=0.1, top=0.95, left=0.1, right=0.95)
 
-    plt.show()
+    if filename:
+        plt.savefig(filename, dpi=500)
+    else:
+        plt.show()
+
+
+def comparePlayerSharpness(sharpChanges: list, players: list, filename: str = None):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    colors = ['#689bf2','#f8a978', '#7ed3b2', '#ff87ca', '#beadfa']
+
+    for i, sc in enumerate(sharpChanges):
+        maxMove = max([s[1] for s in sc])
+        moveSharpAvg = [sum([s[0] for s in sc if s[1] == move])/len([s[0] for s in sc if s[1] == move]) for move in range(1, maxMove+1)]
+
+        ax.plot(range(1, maxMove+1), moveSharpAvg, color=colors[i], label=players[i])
+
+    plt.axhline(0, color='black', linewidth=0.5)
+    ax.set_xlim(1, 40)
+    ax.set_ylim(-0.05, 0.05)
+    ax.set_facecolor('#e6f7f2')
+    ax.set_xlabel('Move Number')
+    ax.set_ylabel('Sharpness Change')
+    ax.legend()
+    plt.subplots_adjust(bottom=0.1, top=0.95, left=0.1, right=0.95)
+
+    if filename:
+        plt.savefig(filename, dpi=500)
+    else:
+        plt.show()
 
 
 if __name__ == '__main__':
     pgns = ['../out/games/tal-bot-sac.pgn']
     sharpChange = sharpChangeByColor(pgns)
     # print(sharpChange)
-    plotGameSharpness('../out/games/tal-bot-sac.pgn')
+    # plotGameSharpness('../out/games/byrne-fisher.pgn')
+    plotGameSharpness('../out/games/kasparov-topalov.pgn', '../out/kasparov-topalov_sharp.png')
+    plotGameSharpness('../out/games/tal-bot-sac.pgn', '../out/tal-botvinnik_sharp.png')
     tal = ['../out/games/tal1959-1962-out.pgn']
     bot = ['../out/games/botvinnik1959-1962-out.pgn']
     scTal = sharpChangeForPlayer(tal, 'Tal, M')
     scBot = sharpChangeForPlayer(bot, 'Botvinnik')
-    plotPlayerSharpness(scTal)
-    plotPlayerSharpness(scBot)
+    # plotPlayerSharpness(scTal)
+    # plotPlayerSharpness(scBot)
+    comparePlayerSharpness([scTal, scBot], ['Tal', 'Botvinnik'], '../out/tal-bot-players_sharp.png')
