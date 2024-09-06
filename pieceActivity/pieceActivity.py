1e48a286acb1bd295820b84e6c85410a7cd0b997
Julian Kern <juliankern323@gmail.com>
2024-09-06 11:21:02 +0200

    added game graphs to piece activity

--- pieceActivity/pieceActivity.py
+++ pieceActivity/pieceActivity.py
@@ -1,4 +1,6 @@
 import chess
+from chess import pgn
+import matplotlib.pyplot as plt
 
 
 def calculatePieceActivity(fen: str) -> list:
@@ -29,6 +31,49 @@ def calculatePieceActivity(fen: str) -> list:
     return activity
 
 
+def plotPieceActivity(pgnPath: str, filename: str = None):
+    """
+    This function plots the piece activity for White and Black in a given game.
+    pgnPath: str
+        Path to the PGN file of the game
+    filname: str
+        The name of the file to which the graph will be saved.
+        If no name is given, the graph will be shown instead of saved.
+    """
+    white = list()
+    black = list()
+    with open(pgnPath, 'r') as pgn:
+        game = chess.pgn.read_game(pgn)
+        board = game.board()
+        for move in game.mainline_moves():
+            board.push(move)
+            activity = calculatePieceActivity(board.fen())
+            white.append(activity[0])
+            black.append(activity[1])
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(range(1, len(white)+1), white, color='#f8a978', label='White piece activity')
+    ax.plot(range(1, len(black)+1), black, color='#111111', label='Black piece activity')
+
+    ax.set_facecolor('#e6f7f2')
+    ax.set_xlabel('Move number')
+    ax.set_ylabel('Piece activity')
+    ax.set_xlim(1, len(white))
+    ax.set_ylim(0)
+    ax.set_xticks(list(range(1, len(white)))[::10])
+    ax.set_xticklabels([i//2 for i in range(len(white))[::10]])
+    plt.subplots_adjust(bottom=0.1, top=0.95, left=0.1, right=0.95)
+
+    ax.legend()
+
+    if filename:
+        plt.savefig(filename, dpi=500)
+    else:
+        plt.show()
+
+
 if __name__ == '__main__':
     fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-    print(calculatePieceActivity(fen))
+    pgn = '../resources/steinitz-vonBardeleben.pgn'
+    plotPieceActivity(pgn)
+    plotPieceActivity('../resources/jinshi-ding.pgn')
