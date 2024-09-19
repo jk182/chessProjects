be69492be23b1ba6fbf1febcdead76a51fa4dc23
Julian Kern <juliankern323@gmail.com>
2024-09-19 17:23:55 +0200

    heat maps for all pieces; visual changes

--- pieceHeatmaps/pieaceHeatmaps.py
+++ pieceHeatmaps/pieaceHeatmaps.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def getPieceData(pgnPath: str, pieceType: int, color: bool, minMove: int = 0, maxMove: int = None) -> list:
+def getPieceData(pgnPath: str, pieceType: int, color: bool, minMove: int = 0, maxMove: int = 200) -> list:
     """
     This function gets the relative number of moves a piece spent on each square in the PGN
     pgnPath: str
@@ -19,21 +19,20 @@ def getPieceData(pgnPath: str, pieceType: int, color: bool, minMove: int = 0, ma
     squares = [0] * 64
     totalMoves = 0
 
-    if not maxMove:
-        maxMove = 300
-
     with open(pgnPath, 'r') as pgn:
         while game := chess.pgn.read_game(pgn):
             board = game.board()
+            gameMoves = 0
             for move in game.mainline_moves():
                 board.push(move)
-                # This should count pieces and moves twice, which should cancel out
-                totalMoves += 1
-                if (totalMoves/2) >= minMove:
-                    for square in board.pieces(pieceType, color):
-                        squares[square] += 1
-                if (totalMoves / 2) >= maxMove:
-                    break
+                if not board.turn == color:
+                    if gameMoves >= minMove:
+                        for square in board.pieces(pieceType, color):
+                            squares[square] += 1
+                    totalMoves += 1
+                    gameMoves += 1
+                    if gameMoves >= maxMove:
+                        break
 
     return [s/totalMoves for s in squares]
 
@@ -49,9 +48,36 @@ def plotHeatmap(pieceData: list):
     plt.show()
 
 
+def plotPieceHeatmaps(pgnPath: str, color: bool, minMove: int = 0, maxMove: int = 200):
+    fig, ((pawn, knight), (bishop, rook), (queen, king)) = plt.subplots(3, 2, figsize=(6, 8))
+    ax = [pawn, knight, bishop, rook, queen, king]
+    files = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    ranks = [8, 7, 6, 5, 4, 3, 2, 1]
+    pieceNames = ['Pawn', 'Knight', 'Bishop', 'Rook', 'Queen', 'King']
+    for p in range(chess.PAWN, chess.KING+1):
+        data = getPieceData(pgnPath, p, color, minMove, maxMove)
+        # Mapping the chessboard to the output
+        data = np.reshape(list(reversed(data)), (8, 8))
+        data = [list(reversed(l)) for l in data]
+        ax[p-1].imshow(data, cmap='plasma')
+        ax[p-1].set_title(pieceNames[p-1])
+
+        ax[p-1].set_xticks(np.arange(8), labels=files)
+        ax[p-1].set_yticks(np.arange(8), labels=ranks)
+        ax[p-1].set_xticks(np.arange(9)-.5, minor=True)
+        ax[p-1].set_yticks(np.arange(9)-.5, minor=True)
+        ax[p-1].grid(which='minor', color="black", linestyle='-', linewidth=1)
+
+        # annotating the heatmap (it looks horrible for now)
+        """
+        for i in range(8):
+            for j in range(8):
+                text = ax[p-1].text(j, i, round(data[i][j], 2), ha="center", va="center", color="w")
+        """
+    fig.tight_layout()
+    plt.show()
+
+
 if __name__ == '__main__':
     pgn = '../resources/carlsbad.pgn'
-    wKnightData = getPieceData(pgn, chess.KNIGHT, chess.WHITE, minMove=5, maxMove=20)
-    wBishopData = getPieceData(pgn, chess.BISHOP, chess.WHITE)
-    plotHeatmap(wKnightData)
-    plotHeatmap(wBishopData)
+    plotPieceHeatmaps(pgn, chess.WHITE, minMove=5)
