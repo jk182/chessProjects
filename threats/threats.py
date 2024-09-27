12c86ea6f9fdd9c793d4dfcf2f2c0eb98abb2168
Julian Kern <juliankern323@gmail.com>
2024-09-27 09:53:42 +0200

    Bug fix, now the analysis of a game works

--- threats/threats.py
+++ threats/threats.py
@@ -16,17 +16,18 @@ def findThreats(sf: chess.engine, board: chess.Board) -> list:
     return -> list
         All threat moves as a list.
     """
-    analysisTime = 2
+    analysisTime = 1
     pvs = 3
     wpCutoff = 10
     threats = list()
     POV = not board.turn
+    mate_score = 100000
 
-    startEval = sf.analyse(board, chess.engine.Limit(time=analysisTime))['score'].pov(POV).score()
+    startEval = sf.analyse(board, chess.engine.Limit(time=analysisTime))['score'].pov(POV).score(mate_score=mate_score)
     board.turn = not board.turn
-    threatInfo = sf.analyse(board, chess.engine.Limit(time=analysisTime), multipv=pvs)
+    threatInfo = sf.analyse(board, chess.engine.Limit(time=analysisTime*1.5), multipv=pvs)
     for info in threatInfo:
-        if functions.winP(info['score'].pov(POV).score()) - wpCutoff >= functions.winP(startEval):
+        if functions.winP(info['score'].pov(POV).score(mate_score=mate_score)) - wpCutoff >= functions.winP(startEval):
             threats.append(info['pv'][0].uci())
     return threats
 
@@ -34,12 +35,12 @@ def findThreats(sf: chess.engine, board: chess.Board) -> list:
 if __name__ == '__main__':
     sf = functions.configureEngine('stockfish', {'Threads': '4', 'Hash': '8192'})
     board = chess.Board('rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq - 0 1')
-    # print(findThreats(sf, board))
+    print(findThreats(sf, board))
     with open('../resources/bernstein-capablanca.pgn', 'r') as pgn:
         game = chess.pgn.read_game(pgn)
         board = game.board()
         for move in game.mainline_moves():
             board.push(move)
-            threats = findThreats(sf, board)
+            threats = findThreats(sf, chess.Board(board.fen()))
             print(move.uci(), threats)
     sf.quit()
