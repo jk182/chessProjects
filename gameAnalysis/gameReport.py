46457ddffbc42ccbfacec1c74a4e6b4eaaca05af
Julian Kern <juliankern323@gmail.com>
2024-10-17 20:19:01 +0200

    added more features to game analysis

--- gameAnalysis/gameReport.py
+++ gameAnalysis/gameReport.py
@@ -2,13 +2,15 @@ import os, sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import functions
+from pieceActivity import pieceActivity as pa
+from pieceHeatmaps import pieceHeatmaps as heatmap
 from chess import engine, pgn, Board
 import chess
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def genFigure(figsize: tuple =(10,6), title: str ="", xLabel: str ="", yLabel: str ="") -> tuple:
+def genFigure(figsize: tuple = (10,6), title: str = "", xLabel: str = "", yLabel: str = "") -> tuple:
     """
     Wrapper to generate subplots
     """
@@ -42,7 +44,7 @@ def getComments(pgnPath: str) -> list:
     return scores
 
 
-def plotWDL(scores: list, filename: str =None):
+def plotWDL(scores: list, filename: str = None):
     """
     Plots a WDL graph given the scores from a game
     """
@@ -63,8 +65,60 @@ def plotWDL(scores: list, filename: str =None):
         plt.show()
 
 
+def sharpChangeByColor(scores: list) -> dict:
+    """
+    This function calculates the sharpness change per move for each color in a list of PGN files.
+    scores: list
+        The scores from the game
+    return -> dict
+        A dictionary indexed by color and containing a list of tuples containing the sharpness change and the move number
+    """
+    startSharp = 0.468*0.5
+    lastSharp = startSharp
+
+    sharp = {'white': list(), 'black': list()}
+    for ply, (wdl, cp) in enumerate(scores):
+        curSharp = functions.sharpnessLC0(wdl) * ((1000-max(wdl))/1000) # Rescaled sharpness to prevent high values for winning positions
+        diff = max(-3, min(3, float(curSharp-lastSharp))) # max, min to prevent huge swings
+        lastSharp = curSharp
+        
+        if ply % 2 == 0:
+            sharp['white'].append(diff)
+        else:
+            sharp['black'].append(diff)
+
+    return sharp
+
+
+def plotSharpnessChange(scores: list, filename: str = None):
+    """
+    This function plots the sharpness change for a game with the scores given
+    """
+    sharpChange = sharpChangeByColor(scores)
+    white = sharpChange['white']
+    black = sharpChange['black']
+
+    fig, ax = genFigure(title='Sharpness Change', xLabel='Move Number', yLabel='Sharpness Change')
+    ax.plot(range(1, len(white)+1), white, color='#f8a978', label='White sharpness change')
+    ax.plot(range(1, len(black)+1), black, color='#111111', label='Black sharpness change')
+    plt.axhline(0, color='black', linewidth=0.5)
+    ax.set_xlim(1, len(white)+1)
+    ax.legend()
+
+    if filename:
+        plt.savefig(filename, dpi=400)
+    else:
+        plt.show()
+
+
 if __name__ == '__main__':
     pgn = '../out/games/greatGames.pgn'
     comments = getComments(pgn)
     carlsenNepo = comments[1]
-    plotWDL(carlsenNepo)
+    # plotWDL(carlsenNepo)
+    plotSharpnessChange(carlsenNepo)
+    pa.plotPieceActivity(pgn)
+    heatmapDataW = heatmap.getAllPieceData(pgn, chess.WHITE)
+    heatmapDataB = heatmap.getAllPieceData(pgn, chess.BLACK)
+    heatmap.plotPieceHeatmaps(heatmapDataW, chess.WHITE)
+    heatmap.plotPieceHeatmaps(heatmapDataB, chess.BLACK)
