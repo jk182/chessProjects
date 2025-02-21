296f9b1819c81ab892d6d84150f51ca074be4b91
Julian Kern <juliankern323@gmail.com>
2025-02-21 10:06:40 +0100

    many tries to make tension work

--- tension/tension.py
+++ tension/tension.py
@@ -1,12 +1,13 @@
 import chess
 import chess.pgn
 import matplotlib.pyplot as plt
+import pandas as pd
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import functions
 
-def buildGraph(board: chess.Board) -> dict():
+def buildGraph(board: chess.Board, orphans: bool = True) -> dict():
     """
     This creates the graph of all piece interactions on the board
     """
@@ -14,7 +15,11 @@ def buildGraph(board: chess.Board) -> dict():
     for square in range(64):
         if board.piece_at(square):
             # if board.is_attacked_by(chess.WHITE, square) or board.is_attacked_by(chess.BLACK, square):
-            graph[square] = [s for s in list(board.attacks(square)) if board.piece_at(s)]
+            v = [s for s in list(board.attacks(square)) if board.piece_at(s) and not board.is_pinned(board.turn, square)]
+            if orphans:
+                graph[square] = v
+            elif v:
+                graph[square] = v
             """
             if attacks := [s for s in list(board.attacks(square)) if board.piece_at(s)]:
                 graph[square] = attacks
@@ -35,7 +40,7 @@ def findShortestPaths(graph: dict, start: int, end: int) -> list:
         path = paths.pop(0)
         node = path[-1]
         if node not in graph.keys():
-            print("Piece not found")
+            # print("Piece not found")
             continue
         neighbors = graph[node]
         if end in neighbors:
@@ -48,13 +53,11 @@ def findShortestPaths(graph: dict, start: int, end: int) -> list:
                         retPaths.append(p)
             return retPaths
         for n in neighbors:
-            if n in path:
-                continue
-            newPath = path.copy()
-            newPath.append(n)
-            paths.append(newPath)
+            if n not in path:
+                newPath = path.copy()
+                newPath.append(n)
+                paths.append(newPath)
     return None
-
 
 
 def calculateBC(graph: dict, piece: int) -> float:
@@ -62,18 +65,31 @@ def calculateBC(graph: dict, piece: int) -> float:
     This function calculates the betweenes centrality for a piece given the piece interaction graph
     """
     n = len(graph)
-    BC = 1/((n-1)*(n-2))
+    norm = 1/((n-1)*(n-2))
     s = 0
     for p in graph.keys():
         for q in graph.keys():
-            if p != piece and q != piece:
+            if p != piece and q != piece and p != q:
                 if paths := findShortestPaths(graph, p, q):
                     pathsThroughPiece = len([path for path in paths if piece in path])
                     s += pathsThroughPiece/len(paths)
                     # print(p, q, paths)
                     # if pathsThroughPiece > 0:
                         # print(piece, paths, s)
-    return BC * s
+    return norm * s
+
+
+def calculateBoardFragility(board: chess.Board) -> float:
+    graph = buildGraph(board, False)
+    if len(graph) <= 2:
+        return 0
+    fragility = 0
+    for piece in range(64):
+        if board.piece_at(piece):
+            for attacker in board.attackers(not board.color_at(piece), piece):
+                fragility += calculateBC(graph, piece)
+                break
+    return fragility
 
 
 def calculateGameFragility(pgnPath: str) -> list:
@@ -84,12 +100,23 @@ def calculateGameFragility(pgnPath: str) -> list:
             board = game.board()
             for move in game.mainline_moves():
                 board.push(move)
-                graph = buildGraph(board)
+                graph = buildGraph(board, True)
+                newGraph = dict()
+                for k, v in graph.items():
+                    if v:
+                        newGraph[k] = v
+                    else:
+                        for nv in graph.values():
+                            if k in nv:
+                                newGraph[k] = v
+                                break
                 BC = 0
                 for piece in range(64):
                     if board.piece_at(piece):
-                        if board.attackers(not board.color_at(piece), piece):
-                            BC += calculateBC(graph, piece)
+                        for attacker in board.attackers(not board.color_at(piece), piece):
+                            # if not board.is_pinned(not board.color_at(piece), attacker):
+                            BC += calculateBC(newGraph, piece)
+                            break
                 frag[-1].append(BC)
     return frag
 
@@ -127,12 +154,64 @@ def getEvalChange(pgnPaths: list) -> list:
     return changes
 
 
+def getTensionCPLoss(gameData: pd.DataFrame) -> list:
+    tensionCP = list()
+    minMove = 15
+    for i, row in gameData.iterrows():
+        if i > 5000:
+            break
+        if row["MoveNr"] >= minMove:
+            if row["Color"]:
+                cpLoss = row["EvalBefore"] - row["EvalAfter"]
+            else:
+                cpLoss = -row["EvalBefore"] + row["EvalAfter"]
+            fragility = calculateBoardFragility(chess.Board(row["Position"]))
+            cpLoss = max(0, cpLoss)
+            tensionCP.append((fragility, cpLoss))
+    return tensionCP
+
+
+def plotTensionCPLoss(tensionCP: list):
+    tension = [t[0] for t in tensionCP]
+    CP = [t[1] for t in tensionCP]
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.scatter(tension, CP)
+    ax.set_xlabel('Fragility')
+    ax.set_ylabel('CP Loss')
+    plt.show()
+
+
 if __name__ == '__main__':
-    graph = buildGraph(chess.Board('1bq1r1k1/1p3pp1/1P2b1n1/p1p1p2p/P1PNP1P1/4BP1P/2Q3B1/3R1RK1 b - - 0 25'))
+    board = chess.Board('1bq1r1k1/1p3pp1/1P2b1n1/p1p1p2p/P1PNP1P1/4BP1P/2Q3B1/3R1RK1 b - - 0 25')
+    board = chess.Board('1k3b2/4B2K/5r2/8/8/R7/8/8 w - - 0 1')
+    graph = buildGraph(board, True)
     print(graph)
-    print(findShortestPaths(graph, 14, 28))
-    print(calculateBC(graph, 14))
+    newGraph = dict()
+    for k, v in graph.items():
+        if v:
+            newGraph[k] = v
+        else:
+            for nv in graph.values():
+                if k in nv:
+                    newGraph[k] = v
+                    break
+    print(newGraph)
+    print(calculateBC(newGraph, 52))
+    df = pd.read_pickle('../out/gameDF')
+    # tcp = getTensionCPLoss(df)
+    # plotTensionCPLoss(tcp)
+    # print(findShortestPaths(graph, 14, 28))
+    """
+    BC = 0
+    for piece in range(64):
+        if board.piece_at(piece):
+            for attacker in board.attackers(not board.color_at(piece), piece):
+                if not board.is_pinned(not board.color_at(piece), attacker):
+                    BC += calculateBC(graph, piece)
+                    break
+    print(BC)
+    """
     # frag = calculateGameFragility('../resources/kasparov-topalov.pgn')
-    # frag = calculateGameFragility('../resources/tension.pgn')
-    # plotGameFragility(frag)
-    print(getEvalChange(['../out/games/wijkMasters2025.pgn']))
+    frag = calculateGameFragility('../resources/tension.pgn')
+    plotGameFragility(frag)
+    # print(getEvalChange(['../out/games/wijkMasters2025.pgn']))
