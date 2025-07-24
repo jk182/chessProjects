34c53df8ff16536e32e1ff696c2b37f85cd86978
Julian Kern <juliankern323@gmail.com>
2025-07-24 16:43:53 +0200

    improved pawn cover and open file detection

--- kingSafety/kingSafety.py
+++ kingSafety/kingSafety.py
@@ -1,7 +1,35 @@
 import chess
 
 
-def pawnShield(board: chess.Board) -> list:
+def getPawnCover(board: chess.Board) -> list:
+    """
+    This fucntion gets all the pawns which cover each side's king
+    board: chess.Board
+        The position
+    return -> list
+        A list containing a list of the pawns covering the king for each side
+    """
+    pawns = [list(), list()]
+    for color in [chess.WHITE, chess.BLACK]:
+        if color == chess.WHITE:
+            index = 0
+        else:
+            index = 1
+
+        king = board.king(color)
+        kingFile = chess.square_file(king)
+        files = [kingFile]
+        if kingFile > 0:
+            files.append(kingFile-1)
+        if kingFile < 7:
+            files.append(kingFile+1)
+        for pawn in board.pieces(chess.PAWN, color):
+            if chess.square_file(pawn) in files:
+                pawns[index].append(pawn)
+    return pawns
+
+
+def evaluatePawnCover(board: chess.Board) -> list:
     """
     This evaluates the pawn shield of both sides in a position
     board: chess.Board
@@ -9,29 +37,36 @@ def pawnShield(board: chess.Board) -> list:
     return -> list
         The score of the pawn shield for both sides (only positive)
     """
-    # TODO: add pawns which aren't directly in front of the king
+    pawns = getPawnCover(board)
+    # The values each covering pawn provides for the king
+    coverValues = [3, 2, 1]
+    openFileLoss = [3, 2]
     ks = [0, 0]
     for color in [chess.WHITE, chess.BLACK]:
+        files = set()
+        if color:
+            index = 0
+        else:
+            index = 1
         king = board.king(color)
-        pawns = list()
-        for square in board.attacks(king):
-            if board.piece_type_at(square) == chess.PAWN and board.color_at(square) == color:
-                pawns.append(square)
-        for p in pawns:
-            if color == chess.WHITE:
-                if p >= king + 7 and p <= king + 9:
-                    ks[0] += 1
-                if p == king + 8:
-                    ks[0] += 1
-            else:
-                if p <= king - 7 and p >= king - 9:
-                    ks[1] += 1
-                if p == king - 8:
-                    ks[1] += 1
-        if color == chess.WHITE and king in [0, 7]:
-            ks[0] += 1
-        if color == chess.BLACK and king in [56, 63]:
-            ks[1] += 1
+        kingFile = chess.square_file(king)
+        kingRank = chess.square_rank(king)
+        for pawn in pawns[index]:
+            pawnRank = chess.square_rank(pawn)
+            files.add(chess.square_file(pawn))
+            for off in range(1, 4):
+                if color:
+                    if pawnRank == kingRank+off:
+                        ks[0] += coverValues[off-1]
+                else:
+                    if pawnRank == kingRank-off:
+                        ks[1] += coverValues[off-1]
+        if kingFile not in files:
+            ks[index] -= openFileLoss[0]
+        if kingFile > 0 and kingFile-1 not in files:
+            ks[index] -= openFileLoss[1]
+        if kingFile < 7 and kingFile+1 not in files:
+            ks[index] -= openFileLoss[1]
     return ks
 
 
@@ -44,6 +79,8 @@ def attackingPieces(board: chess.Board) -> list:
         The king saftey score for both colors, which will be negative for the attackers
     """
     ks = [0, 0]
+    # Giving each piece an attacking value
+    pieceValues = [1, 1, 1, 1, 1, 1]
     for color in [chess.WHITE, chess.BLACK]:
         king = board.king(color)
         kingSquares = board.attacks(king)
@@ -58,7 +95,7 @@ def attackingPieces(board: chess.Board) -> list:
                 for square in board.attacks(piece):
                     if square in kingSquares:
                         print(square)
-                        ks[index] -= 1
+                        ks[index] -= pieceValues[pieceType-1]
     return ks
 
 
@@ -66,5 +103,5 @@ if __name__ == '__main__':
     fen = 'rn1qk2r/1p2bpp1/p2pbn2/4p2p/4P3/1NN1BP2/PPPQ2PP/2KR1B1R b kq - 1 10'
     fen2 = 'rnbqkbnr/pppp1p1p/8/8/2B1Pp2/5p2/PPPP2PP/RNBQ1RK1 w kq - 0 6'
     board = chess.Board(fen2)
-    print(pawnShield(board))
-    print(attackingPieces(board))
+    print(evaluatePawnCover(board))
+    # print(attackingPieces(board))
