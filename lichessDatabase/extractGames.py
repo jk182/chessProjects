766e61d4d4585f0ed269b162b1d3e524c466be0c
Julian Kern <juliankern323@gmail.com>
2025-11-17 21:24:19 +0100

    improved game extraction

--- lichessDatabase/extractGames.py
+++ lichessDatabase/extractGames.py
@@ -3,21 +3,22 @@ import chess.pgn
 import io
 
 
-def extractGamesByRating(ratingBands: list, gamesPerBand: int, maxRatingDiff: int = 50):
+def extractGamesByRating(ratingBands: list, gamesPerBand: int, maxRatingDiff: int = 50, outPath: str = '../out/lichessDB/rating'):
     pgns = list()
     pgns = [""] * len(ratingBands)
+    nGames = [1] * len(ratingBands)
     nextIsEnd = False
     pgn = ""
     wElo = None
     bElo = None
     ratingIndex = None
 
-    for i in range(1000):
+    while True:
         line = input()
         pgn = f'{pgn}{line.strip()}\n'
-        if "WhiteElo" in line:
+        if "[WhiteElo " in line:
             wElo = int(line.split('"')[1])
-        if "BlackElo" in line:
+        if "[BlackElo " in line:
             bElo = int(line.split('"')[1])
             if abs(wElo-bElo) <= maxRatingDiff and (wElo+bElo)/2 >= ratingBands[0]:
                 ratingIndex = len(ratingBands)-1
@@ -27,20 +28,36 @@ def extractGamesByRating(ratingBands: list, gamesPerBand: int, maxRatingDiff: in
                         break
             else:
                 ratingIndex = None
+        if '[WhiteTitle "BOT"]' in line or '[BlackTitle "BOT"]' in line:
+            ratingIndex = None
         if not line.strip():
             if nextIsEnd:
-                if ratingIndex is not None:
+                if ratingIndex is not None and nGames[ratingIndex] < gamesPerBand:
                     pgns[ratingIndex] += f'{pgn}\n'
+                    nGames[ratingIndex] += 1
+                    end = True
+                    for g in nGames:
+                        if g < gamesPerBand:
+                            end = False
+                            break
+                    if end:
+                        for i, r in enumerate(ratingBands):
+                            with open(f'{outPath}{r}.pgn', 'w+') as f:
+                                print(pgns[i], file=f)
+                        """
+                        for p in pgns:
+                            f = io.StringIO(p)
+                            while game := chess.pgn.read_game(f):
+                                print(game)
+                        """
+                        print('Finished')
+                        return pgns
                 pgn = ""
                 nextIsEnd = False
             else:
                 nextIsEnd = True
 
-    for p in pgns:
-        f = io.StringIO(p)
-        while game := chess.pgn.read_game(f):
-            print(game)
     return pgns
 
 
-extractGamesByRating([1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800], 1000)
+extractGamesByRating([1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800], 5000)
