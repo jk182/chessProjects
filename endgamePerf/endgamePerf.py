bfb55555f7e8b418fbfc3be0b04a07fc3f20de1a
Julian Kern <juliankern323@gmail.com>
2025-12-19 18:37:08 +0100

    started with endgame performance

--- endgamePerf/endgamePerf.py
+++ endgamePerf/endgamePerf.py
@@ -4,6 +4,11 @@ from chess import pgn
 import time
 import requests
 
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from functions import configureEngine
+
 
 def tablebaseLookup(token: str, fen: str) -> dict:
     """
@@ -83,6 +88,105 @@ def getEndgameMistakes(token: str, pgnPaths: list) -> dict:
     return mistakes
 
 
+def getEndgameType(board: chess.Board) -> str:
+    """
+    This returns the type of endgame on the board.
+    The types are:
+        pawn
+        rook
+        queen
+    """
+    pieces = set(board.piece_map().values())
+    pieces.remove(chess.Piece.from_symbol('K'))
+    pieces.remove(chess.Piece.from_symbol('k'))
+
+    if len(pieces) == 0:
+        return None
+
+    pieces.remove(chess.Piece.from_symbol('P'))
+    pieces.remove(chess.Piece.from_symbol('p'))
+
+    if len(pieces) == 0:
+        return 'pawn'
+
+    if len(pieces) == 2:
+        if chess.Piece.from_symbol('Q') in pieces and chess.Piece.from_symbol('q') in pieces:
+            return 'queen'
+        if chess.Piece.from_symbol('R') in pieces and chess.Piece.from_symbol('r') in pieces:
+            return 'rook'
+        if chess.Piece.from_symbol('N') in pieces and chess.Piece.from_symbol('n') in pieces:
+            return 'knight'
+        if chess.Piece.from_symbol('B') in pieces and chess.Piece.from_symbol('b') in pieces:
+            return 'bishop'
+
+    return None
+
+
+def getEndgamePerformance(pgnPath: str, player: str, sf: chess.engine) -> dict:
+    scores = dict()
+    depth = 22
+    with open(pgnPath, 'r') as pgn:
+        while game := chess.pgn.read_game(pgn):
+            if player in game.headers["White"]:
+                white = True
+            elif player in game.headers["Black"]:
+                white = False
+            else:
+                continue
+
+            if game.headers["Result"] == "1-0":
+                wPoints = 1
+            elif game.headers["Result"] == "1/2-1/2":
+                wPoints = 0.5
+            elif game.headers["Result"] == "0-1":
+                wPoints = 0
+            else:
+                print('Result not found')
+                continue
+
+            lastEndgame = None
+            startScore = None
+            endgames = set()
+            board = game.board()
+            for move in game.mainline_moves():
+                board.push(move)
+                endgame = getEndgameType(board)
+                print(endgame)
+                if endgame == lastEndgame:
+                    continue
+
+                if endgame is not None:
+                    endgames.add(endgame)
+                    info = sf.analyse(board, chess.engine.Limit(depth=depth))
+                    wdl = list(chess.engine.PovWdl.white(info['wdl']))
+                    print(wdl)
+                    if white:
+                        startScore = (wdl[0] + 0.5*wdl[1])/1000
+                    else:
+                        startScore = (wdl[2] + 0.5*wdl[1])/1000
+
+                if lastEndgame is not None:
+                    if lastEndgame not in scores.keys():
+                        scores[lastEndgame] = [0, 0]
+                    scores[lastEndgame][1] += startScore
+                    if white:
+                        scores[lastEndgame][0] += wPoints
+                    else:
+                        scores[lastEndgame][0] += 1-wPoints
+
+                lastEndgame = endgame
+
+            if endgame == lastEndgame and endgame is not None:
+                    if lastEndgame is not None:
+                        if lastEndgame not in scores.keys():
+                            scores[lastEndgame] = [0, 0]
+                        scores[lastEndgame][1] += startScore
+                        if white:
+                            scores[lastEndgame][0] += wPoints
+                        else:
+                            scores[lastEndgame][0] += 1-wPoints
+    return scores
+
 
 if __name__ == '__main__':
     with open('../resources/tbToken', 'r') as tokenFile:
@@ -90,8 +194,16 @@ if __name__ == '__main__':
 
     pos = '8/4k3/8/8/8/8/4P3/4K3 w - - 0 1'
     pos2 = '4k3/8/8/8/8/8/4P3/4K3 w - - 0 1'
+    pos3 = '4rk2/ppp2ppp/8/3nPP2/3P4/8/P2B2PP/2R3K1 w - - 5 25'
+    rook = '1rk5/1p4R1/8/1p4K1/P7/8/8/8 w - - 0 46'
+    pawn = '8/8/1p6/p3kpP1/8/5K2/P1P5/8 b - - 0 39'
     # print(tablebaseLookup(token, pos)['category'])
     # print(tablebaseLookup(token, pos2))
     games = ['../out/games/norwayChessOpen2024-out.pgn', '../out/games/candidates2024-WDL+CP.pgn', '../out/games/2700games2023-out.pgn']
     g2 = ['../out/games/2700games2023-out.pgn']
-    print(getEndgameMistakes(token, g2))
+    # print(getEndgameMistakes(token, g2))
+
+    sf = configureEngine('stockfish', {'Threads': '6', 'Hash': '8192', 'UCI_ShowWDL': 'true'})
+    # print(getEndgamePerformance('../resources/games/Rubinstein-Duras.pgn', 'Rubinstein', sf))
+    print(getEndgamePerformance('../resources/games/Tartakower-Rubinstein.pgn', 'Rubinstein', sf))
+    sf.quit()
