9dd69d2a9a5c61a498f6f807e482f84b0979fae2
Julian Kern <juliankern323@gmail.com>
2026-02-10 19:36:14 +0100

    some additions to reading data for time of day differences

--- lichessDatabase/timeOfDay.py
+++ lichessDatabase/timeOfDay.py
@@ -7,13 +7,54 @@ pgn = ""
 gameNr = 0
 analysedGames = 0
 players = dict()
-while gameNr < 100000:
+blitz = None
+white = None
+black = None
+whiteElo = 0
+blackElo = 0
+analysed = True
+
+while gameNr < 10000000:
     line = input()
     if line.strip():
-        pgn = f"{pgn}{line}\n"
+        # pgn = f"{pgn}{line}\n"
+        if 'White ' in line:
+            white = line.split('"')[1]
+        elif 'Black ' in line:
+            black = line.split('"')[1]
+        elif 'WhiteElo' in line:
+            whiteElo = int(line.split('"')[1])
+        elif 'BlackElo' in line:
+            blackElo = int(line.split('"')[1])
+        elif 'TimeControl' in line:
+            if line.split('"')[1] == '180+0':
+                blitz = True
+            else:
+                blitz = False
+
         if line[0] == '1':
             if "eval" in line:
-                analysedGames += 1
+                if not (white == 'Odirovski' or black == 'Odirovski'):
+                    continue
+                analysed = True
+                if white in players:
+                    players[white] += 1
+                elif whiteElo >= 2200:
+                    players[white] = 1
+
+                if black in players:
+                    players[black] += 1
+                elif blackElo >= 2200:
+                    players[black] = 1
+
+                # analysedGames += 1
+            else:
+                analysed = False
+
+            white = None
+            black = None
+            whiteElo = 0
+            blackElo = 0
             """
             game = chess.pgn.read_game(io.StringIO(pgn))
             if game.variations:
@@ -29,5 +70,6 @@ while gameNr < 100000:
             gameNr += 1
             pgn = ""
 
+
 print(dict(sorted(players.items(), key=lambda item: item[1])))
-print(analysedGames, gameNr)
+# print(analysedGames, gameNr)
